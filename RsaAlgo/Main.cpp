a7c09237a7b748d1688898ccf6487f6b85af466a
cristian harea <cristian.harea@gmail.com>
2023-10-02 21:23:37 +0300

    first

---
+++ RsaAlgo/Main.cpp
@@ -0,0 +1,68 @@
+﻿#include <iostream>
+#include "RsaUtil.h"
+
+using std::cout;
+using std::cin;
+
+int main()
+{
+	unsigned int p, q, e, isPrime, n, lambda, d, numberToEncrypt, encryptResult, decryptResult;
+
+	cout << "Enter the first prime number: ";
+	cin >> p;
+	isPrime = prime(p);
+	if (isPrime == 0)
+	{
+		cout << "\nWrong Input, you must enter a prime number" << std::endl;
+		cin.get();
+		return 0;
+	}
+
+	cout << "\nEnter the second prime number: ";
+	cin >> q;
+	isPrime = prime(q);
+	if (isPrime == 0)
+	{
+		cout << "\nWrong Input, you must enter a prime number";
+		cin.get();
+		return 0;
+	}
+
+	n = p * q;
+	cout << "\nThe compute of n is: " << n << std::endl;
+
+	lambda = lcm(p - 1, q - 1);
+	cout << "The lambda value is: " << lambda << std::endl;
+
+	cout << "Choose a coprime number < " << lambda << ": ";
+	cin >> e;
+
+	if (e > lambda) {
+		cout << "\nWrong Input, you must choose a prime number < " << lambda << std::endl;
+		cin.get();
+		return 0;
+	}
+
+	isPrime = prime(e);
+	if (isPrime == 0)
+	{
+		cout << "\nWRONG INPUT\n";
+		cin.get();
+		return 0;
+	}
+
+	d = modInverse(e, lambda);
+	cout << "The compute of d is: " << d << std::endl;
+
+	cout << "Enter a number to encrypt: ";
+	cin >> numberToEncrypt;
+
+	encryptResult = moduloPow(numberToEncrypt, e, n);
+	cout << "\nThe encrypt number is: " << encryptResult << std::endl;
+
+	decryptResult = moduloPow(encryptResult, d, n);
+	cout << "The decrypt number is: " << decryptResult << std::endl;
+
+	cin.get();
+	return 0;
+}
---
+++ RsaAlgo/RsaUtil.h
@@ -0,0 +1,7 @@
+#pragma once
+
+int lcm(int, int);
+int prime(int);
+int modInverse(int, int);
+int modulo(int, int);
+int moduloPow(int, int, int);
